bcf54415784981ec793b54ecccafbdb83058ea4c
bean <18222997200@163.com>
2019-02-14 10:36:44 +0800

    数据传输

---
+++ src/main/java/org/happybean/netty/transports/PlainNioServer.java
@@ -0,0 +1,69 @@
+package org.happybean.netty.transports;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
+/**
+ * @author wgt
+ * @date 2019-02-13
+ * @description 下面代码是使用Java NIO实现的例子
+ **/
+public class PlainNioServer {
+    public void server(int port) throws Exception {
+        System.out.println("Listening for connections on port " + port);
+        //open Selector that handles channels
+        Selector selector = Selector.open();
+        //open ServerSocketChannel
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        //get ServerSocket
+        ServerSocket serverSocket = serverChannel.socket();
+        //bind server to port
+        serverSocket.bind(new InetSocketAddress(port));
+        //set to non-blocking
+        serverChannel.configureBlocking(false);
+        //register ServerSocket to selector and specify that it is interested in new accepted clients serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
+        while (true) {
+            //Wait for new events that are ready for process. This will block until something happens
+            int n = selector.select();
+            if (n > 0) {
+            //Obtain all SelectionKey instances that received events
+                Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+                while (iter.hasNext()) {
+                    SelectionKey key = iter.next();
+                    iter.remove();
+                    try {
+                        //Check if event was because new client ready to get accepted
+                        if (key.isAcceptable()) {
+                            ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                            SocketChannel client = server.accept();
+                            System.out.println("Accepted connection from " + client);
+                            client.configureBlocking(false);
+                            client.register(selector, SelectionKey.OP_WRITE, msg.duplicate());
+                        }
+                        if (key.isWritable()) {
+                            SocketChannel client = (SocketChannel) key.channel();
+                            ByteBuffer buff = (ByteBuffer) key.attachment();
+                            //write data to connected client
+                            while (buff.hasRemaining()) {
+                                if (client.write(buff) == 0) {
+                                    break;
+                                }
+                            }
+                            client.close();//close client
+                        }
+                    } catch (Exception e) {
+                        key.cancel();
+                        key.channel().close();
+                    }
+                }
+            }
+        }
+    }
+}
